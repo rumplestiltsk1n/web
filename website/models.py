28a8f716b8252f54d541c6bf9d6783c4e3b1edb8
rumplestiltsk1n <79450629+rumplestiltsk1n@users.noreply.github.com>
2022-12-22 18:08:10 +0200

    Add files via upload

---
+++ website/models.py
@@ -0,0 +1,17 @@
+from . import db
+from flask_login import UserMixin
+
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.String(1000))
+    date = db.Column(db.Time(timezone=True), default=db.func.time(db.func.now()))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    first_name = db.Column(db.String(100))
+    notes = db.relationship('Note')  # all user notes
